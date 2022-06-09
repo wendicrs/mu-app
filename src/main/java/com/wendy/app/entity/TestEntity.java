1e6af2060f1486871e7cf82b44c8088c45d1b6b8
wendicrs <wendicosmegismondi@gmail.com>
2022-06-08 21:33:59 -0400

    commit
    first comit

---
+++ src/main/java/com/wendy/app/entity/TestEntity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.wendy.app.entity;
+
+/**
+ * @author ccosme
+ *
+ */
+public class TestEntity {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
